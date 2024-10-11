ecc51c8f145429e8081dc9bc5c3f1c0f82a3019f
Ricarlos Junior <ricarlosjr@hotmail.com>
2024-10-10 23:29:03 -0300

    Atualização projeto front-end

--- projeto-sigep-front/src/app/components/vendas/vendasdetails/vendasdetails.component.ts
+++ projeto-sigep-front/src/app/components/vendas/vendasdetails/vendasdetails.component.ts
@@ -1,29 +1,40 @@
-import { VendasprodutosdetailsComponent } from './../../vendasprodutos/vendasprodutosdetails/vendasprodutosdetails.component';
 import { VendaProduto } from './../../../models/venda-produto';
 import { VendasService } from './../../../services/vendas.service';
-import { Component, inject } from '@angular/core';
+import { Component, EventEmitter, inject, Output, TemplateRef, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
 import { Venda } from '../../../models/venda';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import Swal from 'sweetalert2';
 import { CommonModule } from '@angular/common';
-
+import { MdbModalModule, MdbModalRef, MdbModalService } from 'mdb-angular-ui-kit/modal';
+import { Produto } from '../../../models/produto';
+import { ProdutosService } from '../../../services/produtos.service';
 
 @Component({
   selector: 'app-vendasdetails',
   standalone: true,
-  imports: [MdbFormsModule, FormsModule, CommonModule, RouterLink, VendasprodutosdetailsComponent],
+  imports: [MdbFormsModule, FormsModule, CommonModule, RouterLink, MdbModalModule],
   templateUrl: './vendasdetails.component.html',
   styleUrl: './vendasdetails.component.scss'
 })
 export class VendasdetailsComponent {
+
+
+ //ELEMENTOS DA MODAL
+ modalService = inject(MdbModalService); // para conseguir abrir a modal
+ @ViewChild("modalVendaProduto") modalVendaProduto!: TemplateRef<any>;
+ modalRef!: MdbModalRef<any>;
+
 
   venda:Venda = new Venda();
   router = inject(ActivatedRoute);
   vendasService = inject(VendasService);
   routerNavegacao = inject(Router);
 
+  vendaProduto: VendaProduto = new VendaProduto();
+  produtos: Produto[] = [];
+  produtosService = inject(ProdutosService);
 
 
   constructor(){
@@ -34,21 +45,37 @@ export class VendasdetailsComponent {
   }
 
   abrirModal() {
-    const modal = document.getElementById('produtoVendaModal');
-    if (modal) {
-      modal.classList.add('show');
-      modal.style.display = 'block';
-    }
+    this.listarProdutos();
+    this.modalRef = this.modalService.open(this.modalVendaProduto);
   }
 
-  vendaProdutoSelecionado(vendaProduto: any) {
-    console.log('venda Produto selecionado:', vendaProduto);
-    this.venda.vendaProdutos.push(vendaProduto);
-
+  adicionarVendaProduto() {
+    this.venda.vendaProdutos.push(this.vendaProduto);
+    this.fecharModal();
   }
+
+  fecharModal() {
+    this.modalRef.close();
+  }
+
 
   excluirVendaProduto(vendaProduto: VendaProduto){
 
+  }
+
+  listarProdutos(){
+    this.produtosService.listar().subscribe({
+      next: produtos => {
+        this.produtos = produtos;
+      },
+      error: erro => {
+        Swal.fire({
+          title: 'Ocorreu um erro inesperado.',
+          icon: 'error',
+          confirmButtonText: 'Ok',
+        });
+      }
+    });
   }
 
    criar(){
